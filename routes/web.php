81e2fc85b669cdde48b3e8ceec96d5ee7baa88c5
duythien2610 <33doduythien@gmail.com>
2026-04-07 08:44:01 +0700

    Add movie admin pages with soft delete

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController3;
 
 Route::get('/', [App\Http\Controllers\MovieController::class, 'index']);
+
+Route::prefix('admin/movies')->name('admin.movies.')->group(function () {
+    Route::get('/', [MovieController3::class, 'index'])->name('index');
+    Route::get('/create', [MovieController3::class, 'create'])->name('create');
+    Route::post('/', [MovieController3::class, 'store'])->name('store');
+    Route::get('/{id}', [MovieController3::class, 'show'])->name('show');
+    Route::delete('/{id}', [MovieController3::class, 'destroy'])->name('destroy');
+});
